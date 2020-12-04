e8bad5df0a533279236df179a70908764e1c2b4a
Rakonczai Adam <adam.rakonczai97@gmail.com>
2020-12-04 15:40:52 +0100

    review app test commit

--- src/main/java/hu/dreamteam/lux_rest/controller/RegisterController.java
+++ src/main/java/hu/dreamteam/lux_rest/controller/RegisterController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RegisterController {
     // comment for test commit
+    // review app test
     private UserService userService;
     private PasswordEncoder passwordEncoder;
 
