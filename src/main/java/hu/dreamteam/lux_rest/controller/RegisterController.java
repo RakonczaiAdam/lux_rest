481564ae5726b73300d4870c58fe9d68cd2647ec
Rakonczai Adam <adam.rakonczai97@gmail.com>
2020-12-16 13:56:45 +0100

    cors

--- src/main/java/hu/dreamteam/lux_rest/controller/RegisterController.java
+++ src/main/java/hu/dreamteam/lux_rest/controller/RegisterController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +28,7 @@ public class RegisterController {
         this.passwordEncoder = passwordEncoder;
     }
 
+    @CrossOrigin(origins = {"http://localhost:3000"})
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody User user){
         try {
